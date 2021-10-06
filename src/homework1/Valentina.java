8e49c950b8c459cab07ef818a317191bc4cf9c19
Valentina08 <ubushv@gmail.com>
2021-10-06 15:58:31 -0400

    update, Valentina

--- src/homework1/Valentina.java
+++ src/homework1/Valentina.java
@@ -3,5 +3,6 @@ package homework1;
 public class Valentina {
     public static void main(String[] args) {
         System.out.println("Valentina");
+        System.out.println("Cybertek");
     }
 }
