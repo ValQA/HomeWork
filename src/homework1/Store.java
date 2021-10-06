098538ac8453f392b8594bb04f59e935847306e6
Valentina08 <ubushv@gmail.com>
2021-10-06 11:51:28 -0400

    addition,feature-c

--- src/homework1/Store.java
+++ src/homework1/Store.java
@@ -3,5 +3,6 @@ package homework1;
 public class Store {
     public static void main(String[] args) {
         System.out.println("Store");
+        System.out.println("Store sells a Red Hat");
     }
 }
