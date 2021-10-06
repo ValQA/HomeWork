1068f89f9d3efdc9c034d6ad21319da62d70f739
Valentina08 <ubushv@gmail.com>
2021-10-06 11:52:16 -0400

    addition2,feature-c

--- src/homework1/Store.java
+++ src/homework1/Store.java
@@ -4,5 +4,6 @@ public class Store {
     public static void main(String[] args) {
         System.out.println("Store");
         System.out.println("Store sells a Red Hat");
+        System.out.println("The red hat is no sale in the Store");
     }
 }
