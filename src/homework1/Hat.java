1922f86702fdc35671f7daacad9fc19e85ad99f4
Valentina08 <ubushv@gmail.com>
2021-10-06 11:47:21 -0400

    addition, feature-b

--- src/homework1/Hat.java
+++ src/homework1/Hat.java
@@ -4,5 +4,6 @@ public class Hat {
     public static void main(String[] args) {
         System.out.println("Hat");
         System.out.println("Red Hat");
+        System.out.println("Black Hat");
     }
 }
